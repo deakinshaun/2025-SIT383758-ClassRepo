c69c6d0a7b07ea09690c74f68c6eab4eb81c9a02
Vikram <thepractitioner.gg@gmail.com>
2025-03-18 14:27:42 +1100

    Week-3 class work
    Created new unity project, added photon lib, created simple character movement for the avatar.

---
+++ VikramStuff/Class3-SIT758/Assets/Scriptts/SinglePlayerMovement.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class SinglePlayerMovement : MonoBehaviour
+{
+    private InputSystem_Actions _controls;
+    private float _turnSpeed=80;
+    private float _moveSpeed=3;
+
+    void Start()
+    {
+        _controls = new InputSystem_Actions();
+        _controls.Enable();
+    }
+
+
+    void Update()
+    {
+        float h = _controls.Player.Move.ReadValue<Vector2>().x;
+        float v = _controls.Player.Move.ReadValue<Vector2>().y;
+        transform.rotation *= Quaternion.AngleAxis(h*_turnSpeed*Time.deltaTime, Vector3.up);
+        transform.position += v * _moveSpeed * Time.deltaTime*transform.forward;
+    }
+}
