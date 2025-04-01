8e8d0c11d76dc6313ec8c7d0c90483c7b830c47d
Shaun <Shaun>
2025-04-01 17:02:56 +1100

    Added week 5 components

---
+++ ShaunsStuff/SIT383W5/Assets/Scripts/GPSDisplay.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using TMPro;
+
+public class GPSDisplay : MonoBehaviour
+{
+    public TextMeshProUGUI textElement;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        float lat;
+        float lon;
+        float alt;
+
+        GetComponent<GPSTracking> ().retrieveLocation (out lat, out lon, out alt);
+        textElement.text = "Latitude: " + lat + ", longitude: " + lon + ", altitude: " + alt;
+    }
+}
